9241b7c96993a6a1a36b55ecf2c7eb6f2ad35d02
Neha Ishwar <neha@bluedot.io>
2019-11-29 14:05:15 +1100

    Bluedot PointSDK integrated

--- android/src/main/java/com/reactlibrary/BluedotPointSdkModule.java
+++ android/src/main/java/com/reactlibrary/BluedotPointSdkModule.java
@@ -4,14 +4,20 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import au.com.bluedot.point.ServiceStatusListener;
+import au.com.bluedot.point.net.engine.BDError;
+import au.com.bluedot.point.net.engine.ServiceManager;
+import au.com.bluedot.point.net.engine.ZoneInfo;
 
-public class BluedotPointSdkModule extends ReactContextBaseJavaModule {
+public class BluedotPointSdkModule extends ReactContextBaseJavaModule implements ServiceStatusListener {
 
     private final ReactApplicationContext reactContext;
+    ServiceManager serviceManager;
 
     public BluedotPointSdkModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        serviceManager = ServiceManager.getInstance(reactContext);
     }
 
     @Override
@@ -23,5 +29,21 @@ public class BluedotPointSdkModule extends ReactContextBaseJavaModule {
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    @Override public void onBlueDotPointServiceStartedSuccess() {
+
+    }
+
+    @Override public void onBlueDotPointServiceStop() {
+
+    }
+
+    @Override public void onBlueDotPointServiceError(BDError bdError) {
+
+    }
+
+    @Override public void onRuleUpdate(List<ZoneInfo> list) {
+
     }
 }
