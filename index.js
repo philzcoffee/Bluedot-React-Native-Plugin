4e5a21e2f63978d76bab5790da7b075ab4d06d05
Neha Ishwar <neha@bluedot.io>
2019-12-18 14:12:08 +1100

    exports index funcations

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
-const { BluedotPointSDK } = NativeModules;
+const eventEmitter = new NativeEventEmitter(NativeModules.BluedotPointSDK)
 
-export default BluedotPointSDK;
+const authenticate = (apiKey, authorizationLevel, onSucessCallback, onFailCallback) => {
+    NativeModules.BluedotPointSDK.authenticate(apiKey, authorizationLevel, onSucessCallback, onFailCallback)
+}
+
+const logOut = (onSucessCallback, onFailCallback) => {
+    NativeModules.BluedotPointSDK.logOut(onSucessCallback, onFailCallback)
+}
+
+const setCustomEventMetaData = (eventMetaData) => {
+    NativeModules.BluedotPointSDK.setCustomEventMetaData(eventMetaData)
+}
+
+const setNotificationIdResourceId = (resourceId) => {
+    NativeModules.BluedotPointSDK.setNotificationIDResourceID(resourceId)
+}
+
+const setForegroundNotification = (channelId, channelName, title, content, targetAllApis) => {
+    NativeModules.BluedotPointSDK.setForegroundNotification(channelId, channelName, title, content, targetAllApis)
+}
+
+const on = (eventName, callback) => {
+    eventEmitter.addListener(eventName, callback)
+}
+
+const unsubscribe = (eventName, callback) => {
+    eventEmitter.removeListener(eventName, callback)
+}
+
+const BluedotPointSDK = {
+    authenticate,
+    logOut,
+    on,
+    unsubscribe,
+    setForegroundNotification,
+    setCustomEventMetaData,
+    setNotificationIdResourceId
+}
+
+export default BluedotPointSDK
